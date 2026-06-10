2862e2af28e9d50c1cdce1329ff7cd2c427f10ee
kennyboba <126933786+kennyboba@users.noreply.github.com>
2026-06-10 23:24:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,21 @@ import os
 from telegram.ext import Application, MessageHandler, CommandHandler, filters
 
 TOKEN = os.environ.get("BOT_TOKEN")
-FILE_PATH = "/app/data/hits.txt"
+FILE_PATH = "hits.txt"
 
 async def save(update, context):
     msg = update.message
     text = msg.text or msg.caption or ""
     line = f"[{msg.date}] {text}\n"
     
-    os.makedirs("/app/data", exist_ok=True)
     with open(FILE_PATH, "a", encoding="utf-8") as f:
         f.write(line)
 
 async def get_file(update, context):
     if os.path.exists(FILE_PATH):
         await update.message.reply_document(FILE_PATH)
+        os.remove(FILE_PATH)
+        await update.message.reply_text("File sent and cleared. Fresh start for new hits.")
     else:
         await update.message.reply_text("No hits logged yet.")
 
